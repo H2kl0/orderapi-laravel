04ab4ed545c46852969d1a5eef0e6a53c88a67c5
JUAN FERNANDO VELASQUEZ <juanchibiris80@gmail.com>
2025-08-28 12:50:42 -0500

    Se creo el metodo index
    Se creo el index y store para causal

---
+++ orderapi/app/Http/Controllers/CausalController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Causal;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class CausalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $causals = Causal::all();
+        return response()->json($causals,Response::HTTP_OK );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function applyValidator(Request $request){
+     $validator = Validator::make($request->all(), $this->rules());
+     $validator->setAttributeNames($this->traductionAttributes());
+     $data = [];
+     if($validator->fails()){
+        $data = response()->json([
+            'errors' => $validator->errors(),
+            'data' => $request->all()
+        ], Response::HTTP_BAD_REQUEST);
+    }
+    return $data;
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->applyValidator($request);
+        if(!empty($data)){
+            return $data;
+        }
+        $causal = Causal::create($request->all());
+        return [
+            'message'=> 'Registro creado exitosamente',
+            'causal'=> $causal       
+        ];
+        return response()->json($response, Response::HTTP_CREATED);
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
